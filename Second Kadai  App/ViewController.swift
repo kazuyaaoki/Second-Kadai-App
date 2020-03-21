831967ccbf1872314cfd167495961b6551f9d228
kazu <kazuya.010928@gmail.com>
2020-03-21 20:15:50 +0900

    Initial Commit

---
+++ Second Kadai  App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Second Kadai  App
+//
+//  Created by Kazuya Aoki on 2020/03/21.
+//  Copyright © 2020 Kazuya Aoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
