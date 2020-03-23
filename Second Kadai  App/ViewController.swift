494094a48193760718c3b3edec111b1f44b1b080
kazu <kazuya.010928@gmail.com>
2020-03-24 00:41:38 +0900

    画面遷移ができるようにした。

--- Second Kadai  App/ViewController.swift
+++ Second Kadai  App/ViewController.swift
@@ -3,18 +3,39 @@
 //  Second Kadai  App
 //
 //  Created by Kazuya Aoki on 2020/03/21.
-//  Copyright © 2020 Kazuya Aoki. All rights reserved.
-//
+//  Copyright © 2020 Kazuya Aoki. All rights reserved.//
+
+
 
 import UIKit
-
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+      
+        
+        let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
+        resultViewController.name = textField.text!
+        
+    }
+        
+        
+        
+   @IBAction func unwind(_ segue: UIStoryboardSegue){
+            }
+            
+        
+    }
+   
 
-
-}
 
